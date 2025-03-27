b51f5d8d6ae592ff3c9d49643db3fe8057ecbef5
AdnanAzem <adnan.azem.250@gmail.com>
2025-03-27 19:13:14 +0200

    finish xp W8 D2

---
+++ Week8/Day2/ExercisesXP/xp/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+// import CarInfo from "./components/Car";
+import ClickMe from "./components/Event";
+import Phone from './components/Phone';
+// import Phone from "./components/Phone";
+// import Color from "./components/Color";
+
+
+function App() {
+
+// Exercice 1
+
+  // const carinfo = {name: "Ford", model: "Mustang"}
+
+  // return (
+  //   <div>
+  //     <header>
+  //     <CarInfo car={carinfo}/>
+  //     </header>
+  //   </div>
+  // );
+
+
+// Exercice 2
+
+// return(
+//   <ClickMe/>
+// )
+
+
+// }
+
+//Exercice 3
+return(
+  <Phone/>
+)
+
+// Exercice 4
+
+// return(
+//   <Color/>
+// )
+
+
+}
+
+export default App;
